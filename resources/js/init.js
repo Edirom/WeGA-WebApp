a7dcaefb67d1eee2dbb4a6c2dcac11d90609bc1c
Peter Stadler <stadlerpeter@yahoo.fr>
2016-10-07 12:15:10 +0200

    removed unused code and added some comments

--- resources/js/init.js
+++ resources/js/init.js
@@ -578,52 +578,13 @@ function getID() {
     return $('.breadcrumb li:last').html()
 };
 
-/* Various functions */
-function showEntries(that)
-{
-    $("#filter span").removeClass("activeFilterElement");
-    $(that).addClass("activeFilterElement");
-};
-
-function changeIconCollapse(that)
-{
-    if ($(that).children("i").first().hasClass("fa-caret-up"))
-    {
-        $(that).children("i").first().removeClass("fa-caret-up");
-        $(that).children("i").first().addClass("fa-caret-down");
-
-        $(that).removeClass("inner-shadow-light");
-        $(that).addClass("gradient-light");
-
-    }
-
-    else if ($(that).children("i").first().hasClass("fa-caret-down"))
-    {
-        $(that).children("i").first().removeClass("fa-caret-down");
-        $(that).children("i").first().addClass("fa-caret-up");
-
-        $(that).removeClass("gradient-light");
-        $(that).addClass("inner-shadow-light");
-    }
-
-    else if ($(that).children("i").first().hasClass("fa-plus-circle"))
-    {
-        $(that).children("i").first().removeClass("fa-plus-circle");
-        $(that).children("i").first().addClass("fa-minus-circle");
-    }
-
-    else if ($(that).children("i").first().hasClass("fa-minus-circle"))
-    {
-        $(that).children("i").first().removeClass("fa-minus-circle");
-        $(that).children("i").first().addClass("fa-plus-circle");
-    }
-};
-
+/* Add search option for advanced search */
 function addSearchOption(that)
 {
     $(that).closest(".col-md-9").append("<div class='searchform'>"+$(that).closest(".searchform").html()+"</div>");
 }
 
+/* Development only: request a new ID */
 $('#create-newID').on('click', function() {
     $('#newID-result span').hide();
     $('#newID-result i').show();
