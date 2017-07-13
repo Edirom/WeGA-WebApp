Edirom/WeGA-WebApp

commit 4d7338400ac08c6c4324389729622fc4f1830640

added display of figures for news

modules/app.xqm
@@ -1162,7 +1162,7 @@ declare
         let $xslt1 := 
             switch($docType)
             case 'letters' return doc(concat($config:xsl-collection-path, '/letters.xsl'))
-            case 'news' case 'writings' case 'documents' return doc(concat($config:xsl-collection-path, '/document.xsl'))
+            case 'news' case 'writings' case 'documents' return doc(concat($config:xsl-collection-path, '/var.xsl'))
             case 'diaries' return doc(concat($config:xsl-collection-path, '/diary_tableLeft.xsl'))
             default  return doc(concat($config:xsl-collection-path, '/var.xsl'))
         let $xslt2 :=

catalogues/options.xml
@@ -2,6 +2,8 @@
     <div xml:id="htmlOptions">
         <!-- where to make the sitemap files accesible from the outside -->
         <entry xml:id="html_sitemapDir" type="xs:string">sitemap</entry>
+        <!-- the max height for inline figures; can be overwritten by @rend='maxSize' on tei:figure -->
+        <entry xml:id="figureHeight" type="xs:int">75</entry>
     </div>
     <div xml:id="dbPaths">
         <!-- where to store the temporary files -->

resources/less/gui.less
@@ -324,7 +324,7 @@ div.table-wrapper {
 .maxSize img {
     display:block;
     max-width: 100%;
-    margin: auto;
+    margin: @line-height-computed auto;
 }
 
 .float-left img {

commit 141d59916d55697ae909328a7875c03c08f38ce5

fixed a bug introduced in 4d7338400ac08c6c4324389729622fc4f1830640
writings and documents need to be transformed with `document.xsl` while
only news should be changed to `var.xsl`

modules/app.xqm
@@ -1168,7 +1168,7 @@ declare
         let $xslt1 := 
             switch($docType)
             case 'letters' return doc(concat($config:xsl-collection-path, '/letters.xsl'))
-            case 'news' case 'writings' case 'documents' return doc(concat($config:xsl-collection-path, '/var.xsl'))
+            case 'writings' case 'documents' return doc(concat($config:xsl-collection-path, '/document.xsl'))
             case 'diaries' return doc(concat($config:xsl-collection-path, '/diary_tableLeft.xsl'))
             default  return doc(concat($config:xsl-collection-path, '/var.xsl'))
         let $xslt2 :=