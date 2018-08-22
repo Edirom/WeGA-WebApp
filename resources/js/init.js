a825d53ef6ab5564b18083225535907ed6e22b9f
Peter Stadler <stadlerpeter@yahoo.fr>
2018-08-22 17:20:37 +0200

    remove empty title from popovers, closes #229
    if you want a title, add it to the HTML template …

--- resources/js/init.js
+++ resources/js/init.js
@@ -340,7 +340,11 @@ $.fn.preview_popover = function() {
             var source = $(response),
                 title = source.find('h3').html(),
                 content = source.children();
-            $('.item-title', container).html(title);
+            // special rule for previews without title, e.g. biblio  
+            if(undefined===title) { $('.item-title', container).remove(); }
+            // default is to copy the non-empty title from the preview
+            else { $('.item-title', container).html(title); }
+            
             $('.item-content', container).html(content);
             $('h3.media-heading', container).remove(); // remove relicts of headings
             
