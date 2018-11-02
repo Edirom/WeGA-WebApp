88189d15cb8e3f47a55b8ca131540981a4642719
Peter Stadler <stadlerpeter@yahoo.fr>
2018-11-02 13:52:44 +0100

    make sure to get distinct labels
    because not all external IIIF manifests provide such distinct labels

--- resources/js/init.js
+++ resources/js/init.js
@@ -786,7 +786,7 @@ function initFacsimile() {
     $.getJSON(manifestUrl, function(data) {
       // For each image create a L.TileLayer.Iiif object and add that to an object literal for the layer control
       $.each(data.sequences[0].canvases, function(_, val) {
-        iiifLayers[val.label] = L.tileLayer.iiif(val.images[0].resource.service['@id'] + '/info.json');
+        iiifLayers[val.label + ' (image ' + (_ + 1) + ')'] = L.tileLayer.iiif(val.images[0].resource.service['@id'] + '/info.json');
       });
         // Add layers control to the map
         L.control.layers(iiifLayers).addTo(map);
