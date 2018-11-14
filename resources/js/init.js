3edf8b0493c5f0f7fa3b9b6e4605d2b1eda899f8
Peter Stadler <stadlerpeter@yahoo.fr>
2018-11-14 11:47:48 +0100

    update initFacsimile() function
    and clean up page.html template

--- resources/js/init.js
+++ resources/js/init.js
@@ -736,11 +736,11 @@ $("#datePicker").initDatepicker();
 
 /* Fieser Hack */
 $('#facsimile-tab').on('click', function() {
-    setTimeout(function() {
-       if ($('#map:visible:not(.leaflet-container)').length !== 0){
+/*    setTimeout(function() {*/
+       if ($('.openseadragon-container').length === 0){
            initFacsimile();
        }
-   }, 500);
+/*   }, 500);*/
 });
 
 /* Load portraits via AJAX on index pages */
@@ -768,7 +768,7 @@ $('.teaser + h2 a').each(function(a,b) {
 
 $('.preview').setTextWrap();
 
-function initFacsimile() {
+/*function initFacsimile() {
     var map,
         iiifLayers = {},
         manifestAttribution,
@@ -800,6 +800,43 @@ function initFacsimile() {
         // Access the first Iiif object and add it to the map
         iiifLayers[Object.keys(iiifLayers)[0]].addTo(map);
     });
+};*/
+
+function initFacsimile() {
+    console.log('initFacsimile')
+    
+    var viewer,
+        iiifLayers = {},
+        manifestAttributions = [],
+        manifestUrls = $('#map').attr('data-url').split(/\s+/);
+    
+    $(manifestUrls).each(function(i,url) {
+        // Grab an IIIF manifest
+        $.getJSON(url, function(data) {
+            // Grab the attribution property from the IIIF manifest
+            manifestAttributions.push(data.attribution);
+        
+            // For each image create a L.TileLayer.Iiif object and add that to an object literal for the layer control
+           /* $.each(data.sequences[0].canvases, function(_, val) {
+                iiifLayers[val.label + ' (image ' + (_ + 1) + ')'] = L.tileLayer.iiif(val.images[0].resource.service['@id'] + '/info.json', {attribution: manifestAttribution});
+            });*/
+            
+        });
+    })
+    
+    viewer = OpenSeadragon({
+        id: "map",
+        prefixUrl: "$resources/lib/openseadragon/build/openseadragon/images/",
+        sequenceMode: true,
+        showRotationControl: true,
+        tileSources: [
+            "https://weber-gesamtausgabe.de/Scaler/IIIF/letters%2FA0407xx%2FA040727%2FA040727_1.jpg/info.json",
+            "https://weber-gesamtausgabe.de/Scaler/IIIF/letters%2FA0407xx%2FA040727%2FA040727_2.jpg/info.json"
+        ]
+    });
+    viewer.addHandler('page', function(page) {
+        console.log(page)
+    })
 };
 
 function jump2diary(dateText) {
