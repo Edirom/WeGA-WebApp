41328d179bdda7cd13d97d3b7fe4f2a0374026af
Peter Stadler <stadlerpeter@yahoo.fr>
2019-10-28 10:05:41 +0100

    remove `window.onload` to fix swagger UI initialization issue

--- resources/js/swagger.js
+++ resources/js/swagger.js
@@ -1,34 +1,29 @@
 $(function () {
-   var url = $('.swagger-section').attr('data-api-base') + "/swagger.json";
-                                  
-             window.onload = function() {
-             
-             // Hide some elements
-             const HideInfoUrlPartsPlugin = () => {
-                return {
-                  wrapComponents: {
-                    InfoUrl: () => () => null,
-                    InfoBasePath: () => () => null // this hides the `Base Url` part too, if you want that
-                  }
-                }
-              }
-             
-               // Begin Swagger UI call region
-               const ui = SwaggerUIBundle({
-                 url: url,
-                 dom_id: '#swagger-ui-container',
-                 deepLinking: true,
-                 presets: [
-                   SwaggerUIBundle.presets.apis,
-                   SwaggerUIStandalonePreset
-                 ],
-                 plugins: [
-                   SwaggerUIBundle.plugins.DownloadUrl,
-                   HideInfoUrlPartsPlugin
-                 ],
-                 layout: "BaseLayout"
-               })
-               // End Swagger UI call region
-               window.ui = ui
-             }
+    var url = $('.swagger-section').attr('data-api-base') + "/swagger.json";
+    
+    // Hide some elements
+    const HideInfoUrlPartsPlugin = () => {
+        return {
+            wrapComponents: {
+                InfoUrl: () => () => null,
+                InfoBasePath: () => () => null // this hides the `Base Url` part too, if you want that
+            }
+        }
+    }
+    
+    // Begin Swagger UI call region
+    const ui = SwaggerUIBundle({
+        url: url,
+        dom_id: '#swagger-ui-container',
+        deepLinking: true,
+        presets:[
+        SwaggerUIBundle.presets.apis,
+        SwaggerUIStandalonePreset],
+        plugins:[
+        SwaggerUIBundle.plugins.DownloadUrl,
+        HideInfoUrlPartsPlugin],
+        layout: "BaseLayout"
+    })
+    // End Swagger UI call region
+    window.ui = ui
 });
