6814ef7799956b355234c8ea404159f990231f67
SteffenAstheimer <steffen.astheimer@uni-paderborn.de>
2025-11-19 17:15:01 +0100

    move calendar date restriction to point of rendering

--- resources/js/init.js
+++ resources/js/init.js
@@ -210,18 +210,12 @@ function getRequestParams() {
  */
 $('.ajax-loader').each(function() {
     const params = getRequestParams(),
-        dateStr = params.get('otdDate'),
-        date = moment(dateStr, "YYYY-MM-DD"),
         url = $(this).attr('data-target') + '?' + params.toString();
     $(this).load(url, function() {
         $('.portrait, .flipcard').initFlipCard(); // necessary for the carousel on the start page
         $(this).removeClass('invisible'); // necessary for the carousel on the start page
         if(this.id === 'ical-events') { // necessary for the ical calendar on the start page
-            const start = moment("2025-11-17", "YYYY-MM-DD"),
-                end   = moment("2026-12-31", "YYYY-MM-DD");
-            if (date.isSameOrAfter(start) && date.isSameOrBefore(end)) { // only load calendar from release 4.13 on until end of 2026
-                init_fullcalendar(params.get('otdDate'), params.get('lang'));
-            }
+            init_fullcalendar(params.get('otdDate'), params.get('lang'));
         };
     });
 });
@@ -1244,8 +1238,8 @@ function init_fullcalendar(initialDate, lang) {
                 if (info.event.url) { window.open(info.event.url); }
             }
         });
-        // only render calendar when events could be retrieved
-        if (icalEvents!==null) {
+        // only render calendar until end of 2026 and when events could be retrieved
+        if (icalEvents!==null && moment(initialDate, "YYYY-MM-DD").isSameOrBefore(moment("2026-12-31", "YYYY-MM-DD"))) {
             calendar.render();
             $('#ical-events').show();
         };
