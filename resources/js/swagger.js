254b4f0451366c29a1f91ba66d26e17ea492c7e4
Peter Stadler <stadlerpeter@yahoo.fr>
2026-04-21 23:38:40 +0200

    refactor code

--- resources/js/swagger.js
+++ resources/js/swagger.js
@@ -1,19 +1,14 @@
-$(function () {
-    var url = $('.swagger-section').attr('data-openapi');
-    
-    // Begin Swagger UI call region
-    const ui = SwaggerUIBundle({
-        url: url,
-        dom_id: '#swagger-ui-container',
-        deepLinking: true,
-        presets:[
+const url = document.querySelector('.swagger-section').getAttribute('data-openapi')
+
+const swaggerUI = SwaggerUIBundle({
+    url: url,
+    dom_id: '#swagger-ui-container',
+    deepLinking: true,
+    presets:[
         SwaggerUIBundle.presets.apis,
         SwaggerUIStandalonePreset],
-        plugins:[
+    plugins:[
         SwaggerUIBundle.plugins.DownloadUrl
-        ],
-        layout: "BaseLayout"
-    })
-    // End Swagger UI call region
-    window.ui = ui
-});
+    ],
+    layout: "BaseLayout"
+})
